ac67ed77fbe197c5abcd4dd3c3628b4f24926e07
allan-softvaro <allan.alvarenga@softvaro.com.br>
2023-04-18 14:50:09 -0300

    feat: add input component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Input } from "../../components/Input";
 
 export function Home() {
   return (
@@ -23,7 +24,9 @@ export function Home() {
         </li>
       </Menu>
 
-      <Search></Search>
+      <Search>
+        <Input placeholder=" Pesquisar pelo título" />
+      </Search>
 
       <Content></Content>
 
