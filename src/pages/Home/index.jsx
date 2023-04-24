7674e32f46235c0a44c812bd0260280f0b544e1f
allan-softvaro <allan.alvarenga@softvaro.com.br>
2023-04-24 14:50:46 -0300

    feat: :sparkles: add section e notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
 import { Input } from "../../components/Input";
+import { Note } from "../../components/Note";
+import { Section } from "../../components/Section";
 
 export function Home() {
   return (
@@ -28,7 +30,19 @@ export function Home() {
         <Input placeholder=" Pesquisar pelo título" />
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
       <NewNote>
         <FiPlus />
